94ba549492e72b2fbe477605b91ca9c5681e8513
John Sheehan <jennasyseng@gmail.com>
2022-01-23 13:41:02 -0800

    Trying to fix npm install skipping files reverted import module instead of function back to function

--- PythonTransformer.js
+++ PythonTransformer.js
@@ -4,7 +4,7 @@ const child_process = require("child_process");
 const path = require('path');
 const fs = require('fs');
 
-const versionUtil = require('./versionUtil')
+const {getVersion} = require('./versionUtil')
 
 
 // This Python transformer was built on the shoulders of the original parcel-plugin-transcrypt package
@@ -39,8 +39,8 @@ const DEFAULT_PACKAGE_CONFIG = {
 
 // Get the versions of Python and Transcrypt being used
 const validateVersions = (command) => {
-    const pythonVersion = versionUtil.getVersion(command, "python");
-    const transcryptVersion = versionUtil.getVersion(command, "transcrypt");
+    const pythonVersion = getVersion(command, "python");
+    const transcryptVersion = getVersion(command, "transcrypt");
     // console.log('Python Version:', pythonVersion);
     // console.log('Transcrypt Version:', transcryptVersion);
 
